a109db44fabda05fea7d1884749577ee0d9e10b0
Ridan77 <dan.ribak17@gmail.com>
2025-07-30 19:17:45 +0300

    add more to details

--- apps/mail/pages/MailDetails.jsx
+++ apps/mail/pages/MailDetails.jsx
@@ -1,35 +1,43 @@
 import { mailService } from "../services/mail.service.js";
 
-const { useParams, useNavigate, Link } = ReactRouterDOM
+const { useParams, useNavigate, Link } = ReactRouterDOM;
 
-const { useState, useEffect } = React
+const { useState, useEffect } = React;
 
 export function MailDetails() {
+  const [mail, setMail] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const params = useParams();
+  const navigate = useNavigate();
+  console.log("in details");
+  useEffect(() => {
+    loadMail();
+  }, [params.mailId]);
 
-    const [mail, setMail] = useState(null)
-    const [isLoading, setIsLoading] = useState(true)
-    const params = useParams()
-    const navigate = useNavigate()
-console.log('in details')
-    useEffect(() => {
-        loadMail()
-    }, [params.mailId])
+  function loadMail() {
+    setIsLoading(true);
+    mailService
+      .get(params.mailId)
+      .then((mail) => setMail(mail))
+      .catch((err) => console.log("err:", err))
+      .finally(() => setIsLoading(false));
+  }
 
-    function loadMail() {
-        setIsLoading(true)
-        mailService.get(params.mailId)
-            .then(mail => setMail(mail))
-            .catch(err => console.log('err:', err))
-            .finally(() => setIsLoading(false))
-    }
+  function onBack() {
+    navigate("/mail");
+  }
 
-    function onBack() {
-        navigate('/mail')
-    }
+  if (isLoading) return <div className="loader">Loading...</div>;
+  const { subject, body, sentAt, from, to } = mail;
 
-    if (isLoading) return <div className="loader">Loading...</div>
-
-
-return <h1>Details</h1>
+  return (
+    <div>
+      <h1>Details</h1>
+      <p>From:{from}</p>
+      <p>To:{to}</p>
+      <p>Subject: {subject}</p>
+      <p>{body}</p>
+      <button onClick={onBack}>Back</button>
+    </div>
+  );
 }
-
