1dd42495e8460cc907e917d51486226e0307d22a
Ohad <ohadfefer16@gmail.com>
2025-07-30 15:40:38 +0300

    Initial commit - rendered 4 notes with a delete button

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -1,3 +1,33 @@
-export function NoteList() {
-    return <div>note list</div>
+const { Link } = ReactRouterDOM
+
+import { NotePreview } from "./NotePreview.jsx";
+
+export function NoteList({ notes, onRemoveNote }) {
+
+    const listAttrs = {
+        className: 'note-list container',
+        title: 'Hello NoteList!',
+        onClick: () => { console.log('List Clicked!') }
+    }
+
+    if (!notes.length) return <div>No Notes To Show...</div>
+    return (
+        <ul {...listAttrs}>
+            {notes.map(note => (
+                <li key={note.id}>
+                    <NotePreview note={note} />
+                    <section>
+                        <button onClick={() => onRemoveNote(note.id)}>
+                            Remove
+                        </button>
+                        <button >
+                            Edit
+                            {/*<Link to={`/note/edit/${note.id}`}>Edit</Link>*/}
+                        </button>
+                    </section>
+                </li>
+            ))}
+        </ul>
+    )
+
 }
