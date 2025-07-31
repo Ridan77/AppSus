3d0872bcddea928d7744362fdf43b1e0eb2f9bc1
Ridan77 <dan.ribak17@gmail.com>
2025-07-31 20:29:48 +0300

    finish more design

--- apps/mail/cmps/SideNav.jsx
+++ apps/mail/cmps/SideNav.jsx
@@ -3,34 +3,34 @@ export function SideNav({ unreadCount, onSetFilterBy, filterBy }) {
     filterBy.folder = folder;
     onSetFilterBy(filterBy);
   }
-
+const folder=filterBy.folder
   return (
     <div className="sidebar-container">
-      <button
+      <button className={folder==='inbox' ? 'selected-folder' : ''}
         onClick={() => {
           onClickFolder("inbox");
         }}>
         Inbox ({unreadCount})
       </button>
-      <button
+      <button className={folder==='starred' ? 'selected-folder' : ''}
         onClick={() => {
           onClickFolder("starred");
         }}>
         Starred
       </button>
-      <button
+      <button className={folder==='sent' ? 'selected-folder' : ''}
         onClick={() => {
           onClickFolder("sent");
         }}>
         Sent
       </button>
-      <button
+      <button className={folder==='draft' ? 'selected-folder' : ''}
         onClick={() => {
           onClickFolder("draft");
         }}>
         Draft
       </button>
-      <button
+      <button className={folder==='trash' ? 'selected-folder' : ''}
         onClick={() => {
           onClickFolder("trash");
         }}>
