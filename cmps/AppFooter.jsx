c53e62507241dd96a07c1f6ac40ebf2400b8d4fb
Ridan77 <dan.ribak17@gmail.com>
2025-08-01 18:27:34 +0300

    more design

---
+++ cmps/AppFooter.jsx
@@ -0,0 +1,10 @@
+const { Link, NavLink } = ReactRouterDOM;
+
+export function AppFooter() {
+  return <footer className="appfooter">
+    
+    <h3>All Rights Reserved ©️</h3>
+    <h3>By Ohad Ferer & Dan Ribak</h3>
+    
+    </footer>;
+}
