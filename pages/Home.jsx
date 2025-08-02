f3043b9c8377e56a3a44891339f6f693ed4858ca
Ridan77 <dan.ribak17@gmail.com>
2025-08-02 16:23:07 +0300

    finish about and integration

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,18 +2,7 @@ import { showSuccessMsg } from "../services/event-bus.service.js";
 
 export function Home() {
   return (
-    <section className="container home">
-      <div className="box">
-        <h2>App Sus</h2>
-        <h2>A Sprint Project</h2>
-        <h2>By</h2>
-        <div className="img-home" >
-            <img src="" alt="" srcset="" />
-            <img src="" alt="" srcset="" />
-        </div>
+    <h1>Home</h1>
 
-
-      </div>
-    </section>
   );
 }
