48c56721dedecb7c5d77272e23cd6799bac67f50
Ridan77 <dan.ribak17@gmail.com>
2025-08-02 13:14:42 +0300

    finish header and hamburger menu

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,16 +1,38 @@
-const { Link, NavLink } = ReactRouterDOM
+const { Link, NavLink } = ReactRouterDOM;
+const { useState, useEffect } = React;
 
 export function AppHeader() {
+  const [menuToggle, setMenuToggle] = useState(false);
 
-    return <header className="app-header">
-        <Link to="/">
-            <h3>App Sus ✨</h3>
-        </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-        </nav>
+
+
+
+function onToggleMenu() {
+    document.querySelector('body').classList.toggle('menu-open')
+   setMenuToggle(menuToggle=>!menuToggle)
+}
+
+  return (
+<header className={`app-header ${menuToggle ? 'menu-open' : ''}`}>
+      <Link to="/">
+        <h3>App Sus ✨</h3>
+      </Link>
+      <div className="header-items">
+
+      <nav className="header-nav-bar">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/mail">Mail</NavLink>
+        <NavLink to="/note">Note</NavLink>
+      </nav>
+        <button className="btn-toggle-menu" onClick={onToggleMenu}>
+          <div className="hamburger-lines">
+            <span className="line line1"></span>
+            <span className="line line2"></span>
+            <span className="line line3"></span>
+          </div>
+        </button>
+      </div>
     </header>
+  );
 }
